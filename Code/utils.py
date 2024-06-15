0cde7a8bce7cfdb6eb52fde29c2131e66d3d20ce
José Zapana <jose.zapana@pucp.edu.pe>
2024-06-15 00:07:54 -0300

    Add latest PPO version, Remove old Rainbow-DQN, Add functional Rainbow-DQN, Add some utility functions

--- Code/utils.py
+++ Code/utils.py
@@ -45,17 +45,16 @@ def set_train_path(models_path_name):
     models_path = os.path.join(os.getcwd(), models_path_name, '')
     os.makedirs(os.path.dirname(models_path), exist_ok=True)
 
-    dir_content = os.listdir(models_path)
+    dir_content = [entry.name for entry in os.scandir(models_path) if entry.is_dir()] #os.listdir(models_path)
     if dir_content:
-        previous_versions = [str(name.split("_")[1]) for name in dir_content]
+        previous_versions = [int(name.split("_")[1]) for name in dir_content if '400' not in name]
         new_version = str(max(previous_versions) + 1)
     else:
         new_version = '1'
 
     data_path = os.path.join(models_path, 'model_'+new_version, '')
     os.makedirs(os.path.dirname(data_path), exist_ok=True)
-    return data_path 
-
+    return data_path
 def get_model_path(models_path_name, model_n):
     model_folder_path = os.path.join(os.getcwd(), models_path_name, 'model_'+str(model_n), '')
     return model_folder_path
